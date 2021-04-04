ef44cf33e7f23c168a0ba0b53b6217ec7b86dc97
Azakharov <azakharov@aligntech.com>
2021-04-04 23:28:54 +0300

    [-] Add support of BOOST and GTEST.
    [TODO] Write short review, on how I did it in CLion.

---
+++ tests/testcase.cpp
@@ -0,0 +1,13 @@
+//
+// Created by azakharov on 4/4/2021.
+//
+
+#include <gtest/gtest.h>
+
+TEST(SuitName, TestName){
+    EXPECT_EQ(1 + 1, 2);
+}
+
+TEST(CompareStrings, Equality){
+    EXPECT_EQ("", "1");
+}
