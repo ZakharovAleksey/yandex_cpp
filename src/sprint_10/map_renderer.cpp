8f1ba7b534324cfb94b718a13fbe5f9f5e265f41
ZakharovAleksey <lehazakharov@yandex.ru>
2021-09-20 11:31:46 +0300

    [s-10] [+] SVG map. Basic prototype for Visualization class.

--- src/sprint_10/map_renderer.cpp
+++ src/sprint_10/map_renderer.cpp
@@ -1,5 +1,36 @@
-//
-// Created by azakharov on 9/19/2021.
-//
-
 #include "map_renderer.h"
+
+#include "geo.h"
+#include "svg.h"
+
+namespace render_settings {
+
+Visualization& Visualization::SetScreen(const Screen& screen) {
+    screen_ = screen;
+    return *this;
+}
+Visualization& Visualization::SetLineWidth(double width) {
+    line_width_ = width;
+    return *this;
+}
+Visualization& Visualization::SetStopRadius(double radius) {
+    stop_radius_ = radius;
+    return *this;
+}
+
+Visualization& Visualization::SetLabels(ObjectType type, Label label) {
+    labels_.emplace(type, label);
+    return *this;
+}
+
+Visualization& Visualization::SetUnderLayer(UnderLayer layer) {
+    under_layer_ = std::move(layer);
+    return *this;
+}
+
+Visualization& Visualization::SetUnderLayer(std::vector<svg::Color> colors) {
+    colors_ = std::move(colors);
+    return *this;
+}
+
+}  // namespace render_settings
--- src/sprint_10/map_renderer.h
+++ src/sprint_10/map_renderer.h
@@ -1,10 +1,51 @@
-//
-// Created by azakharov on 9/19/2021.
-//
+#pragma once
 
-#ifndef YP_CPP1_MAP_RENDERER_H
-#define YP_CPP1_MAP_RENDERER_H
+#include "svg.h"
 
-class map_renderer {};
+namespace render_settings {
 
-#endif  // YP_CPP1_MAP_RENDERER_H
+enum class ObjectType { Bus = 0, Stop = 1 };
+
+// Used to project coordinates on map
+struct Screen {
+    double width_{0.};
+    double height_{0.};
+    double padding_{0.};
+};
+
+// Background color under the names of stops and routes
+struct UnderLayer {
+    svg::Color color_;
+    svg::Color width_;
+};
+
+// Settings to display stops and buses names
+struct Label {
+    int font_size_{0};
+    svg::Point offset_;
+};
+
+//    TODO: Make this class SingleTone
+class Visualization {
+public:
+    Visualization() = default;
+
+    Visualization& SetScreen(const Screen& screen);
+    Visualization& SetLineWidth(double width);
+    Visualization& SetStopRadius(double radius);
+
+    Visualization& SetLabels(ObjectType type, Label label);
+    Visualization& SetUnderLayer(UnderLayer layer);
+    Visualization& SetUnderLayer(std::vector<svg::Color> colors);
+
+private:
+    Screen screen_;
+    double line_width_{0.};
+    double stop_radius_{0.};
+
+    std::unordered_map<ObjectType, Label> labels_;
+    UnderLayer under_layer_;
+    std::vector<svg::Color> colors_;
+};
+
+}  // namespace render_settings
