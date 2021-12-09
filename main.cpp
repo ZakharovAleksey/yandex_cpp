04a97a410247c5f3a62d07ff5d7e5d4bb59eb87f
ZakharovAleksey <lehazakharov@yandex.ru>
2021-12-09 20:33:38 +0300

    [s-15] [+] Initial commit for sprint #15
    [+] Update CMakeLists.txt
    [+] Copy initial code

--- main.cpp
+++ main.cpp
@@ -1,31 +1,24 @@
-#include <chrono>
-#include <fstream>
 #include <iostream>
 
-#include "src/sprint_12/json_reader.h"
-#include "src/sprint_12/request_handler.h"
+#include "src/sprint_15/lexer.h"
+#include "src/sprint_15/test_runner_p.h"
 
-#include "src/sprint_13/vector.h"
-
-using namespace catalogue;
+namespace parse {
+void RunOpenLexerTests(TestRunner& tr);
+}
 
 int main() {
-    //    TODO: почитать аро устройство проекта - как расположить
-    std::ifstream in{"D:\\education\\cpp\\yandex_cpp\\data\\local\\input_route.json"};
-    std::ofstream out{"D:\\education\\cpp\\yandex_cpp\\data\\local\\output_route.json"};
-
     try {
-        auto start = std::chrono::system_clock::now();
-
-        request::ProcessTransportCatalogueQuery(in, out);
-
-        auto end = std::chrono::system_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        std::cout << elapsed.count() << "ms." << '\n';
-    } catch (std::exception& e) {
-        std::cout << "Exception: " << e.what() << std::endl;
+        TestRunner tr;
+        parse::RunOpenLexerTests(tr);
+        parse::Lexer lexer(std::cin);
+        parse::Token t;
+        while ((t = lexer.CurrentToken()) != parse::token_type::Eof{}) {
+            std::cout << t << std::endl;
+            lexer.NextToken();
+        }
+    } catch (const std::exception& e) {
+        std::cerr << e.what();
+        return 1;
     }
-
-    std::cout << "done" << std::endl;
-    return 0;
 }
